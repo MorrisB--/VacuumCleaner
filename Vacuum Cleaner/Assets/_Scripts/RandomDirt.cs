c2b26c30f4d1e23ce69295cf2a45e7706b3a79db
MorrisB-- <morris.ballenger@yahoo.com>
2016-06-06 20:30:45 -0500

    Added new Vacuum Cleaner files

---
+++ Vacuum Cleaner/Assets/_Scripts/RandomDirt.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomDirt : MonoBehaviour {
+
+	public GameObject Dirt;
+	public float DirtGenerateFreq = 5.0f;
+	public bool ReadyToGenerateDirt = false;
+	// Use this for initialization
+	void Start () {
+		Invoke ("Activate", 3.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GenerateDirt ();
+	}
+
+	void Activate()
+	{
+		ReadyToGenerateDirt = true;
+	}
+
+	void GenerateDirt()
+	{
+		if (ReadyToGenerateDirt) {
+			StartCoroutine (WaitToGenerate ());
+			Instantiate (Dirt, new Vector3 (Random.Range (-1.5f, 6.5f), -1, Random.Range (-1.2f, -4.7f)), Quaternion.identity);
+		}
+	}
+
+	IEnumerator WaitToGenerate()
+	{
+		ReadyToGenerateDirt = false;
+		yield return new WaitForSeconds (DirtGenerateFreq);
+		ReadyToGenerateDirt = true;
+	}
+}
---
+++ Vacuum Cleaner/Assets/_Scripts/VacuumAgent.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VacuumAgent : MonoBehaviour {
+
+	public Transform[] Tiles; // Tiles in the scene
+	public Transform CurrentTile; // Agent's current tile
+	public Transform DirtTransform; // Transform component of a dirt object that is percived by the agent
+	public float[] DistancesToTiles; // A float array that keeps the distances to all tiles
+	public float MoveSpeed = 5f; // Agent's movement speed
+	public bool isCleaning = false; // Flag value that indicates whether the agent is in the cleaning process or not
+
+	void Start () {
+		// Initialize agent target destination
+		// your code here...
+
+	}
+	
+	void Update () {
+
+		CurrentTile = CurrentAgentTile (); // Find the curret tile
+
+		if (!isCleaning) { // Currently there is no dirt, agent is moving between tiles
+			/*
+				If the agent is on tile A, it should move to tile B or vice versa
+			// your code here...
+
+
+			*/
+		} else { // Agent notices some dirt and posistioning itself over the dirt.
+			transform.Translate ((DirtTransform.position - transform.position) * Time.deltaTime, Space.World);
+		}
+
+		PerceptSequence (); // This function should print the percept sequence in this format : [A, Clean] or [A, Dirty]
+	}
+
+	Transform CurrentAgentTile() // This function returns the closest tile to the agent. The closest tile is considered as the current tile
+	{
+		for (int i = 0; i < Tiles.Length; i++) {
+			// Calculate distances to tiles
+			// your code here...
+
+
+		}
+
+		int closestTileIndex = 0;
+		float closestDistance = DistancesToTiles [0];
+		for (int i = 0; i < DistancesToTiles.Length; i++) {
+			// find the index of the tile that is closest to the agent
+			// your code here...
+
+		}
+
+		return Tiles [closestTileIndex];
+	}
+		
+	void OnTriggerEnter(Collider col) // This function detects collisions
+	{
+		if (col.tag == "Dirt") { // Agent noticed some dirt
+			Debug.Log ("Agent: Dirt found at " + CurrentTile.name);
+			StartCoroutine (CleanDirt (col.gameObject));
+		} 
+
+	}
+
+	IEnumerator CleanDirt(GameObject dirtGameObject) // Cleaning starts
+	{
+		// Start cleaning process
+		// your code here...
+
+		yield return new WaitForSeconds (2.0f);
+		// End cleaning process
+		// make the dirt disappear
+		// your code here...
+
+	}
+
+	void PerceptSequence()
+	{
+		string cleaningState = "";
+		// if agent noticed any dirt on the current tile, than the cleaningState should have a value "Dirty". Else, it should be "Clean".
+		// your code here...
+
+
+		Debug.Log ("[" + CurrentTile.name + ", " + cleaningState + "]");
+	}
+}
